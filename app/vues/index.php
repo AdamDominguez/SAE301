0bee4600bebbd4f4188a3f76c49baaba3d3b4188
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2025-11-12 10:42:58 +0100

    inital push

---
+++ app/vues/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../public/css/main.css">
+    <link rel="stylesheet" href="../public/css/home.css">
+</head>
+
+<body>
+    <main>
+        <section class="HOME">
+            
+        </section>
+    </main>
+</body>
+
+</html>
