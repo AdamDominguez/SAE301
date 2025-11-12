71648ece5880c9bd122b4914be73e0b687a74da8
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2025-11-12 14:12:22 +0100

    t

--- app/vues/vueAccueil.php
+++ app/vues/vueAccueil.php
@@ -22,13 +22,21 @@ require "header.php";
                     <p>Un système intelligent de surveillance pour vos ruches connectées. Suivez la température, le
                         poids, l'humidité et la santé de vos colonies où que vous soyez.</p>
                     <div class="Boutons">
-                        <div class="Decouvrir"><a href="">Découvrir le tableau de bord</a></div>
+                        <div class="Decouvrir"><a href="">Découvrir le tableau de bord <svg
+                                    xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"
+                                    fill="none">
+                                    <path d="M6.25 15H23.75M23.75 15L15 6.25M23.75 15L15 23.75" stroke="white"
+                                        stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></a></div>
                         <div class="Savoir"><a href="">En savoir plus</a></div>
                     </div>
                 </div>
                 <div class="Droite">
-                    <img src="./public/img/home_droite.avif" alt="Ruche schématisé avec abeilles et éléments graphiques">
+                    <img src="./public/img/home_droite.avif"
+                        alt="Ruche schématisé avec abeilles et éléments graphiques">
                 </div>
+            </div>
+            <div class="HomeHero">
             </div>
         </section>
     </main>
