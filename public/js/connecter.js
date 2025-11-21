0a4c5ea7b9a078aab6c6f9c09ef0a8266fe03ea9
abernier13 <axelbernier330@gmail.com>
2025-11-21 09:51:42 +0100

    Menu connexion
    Le menu s'ouvre et se ferme

--- public/js/connecter.js
+++ public/js/connecter.js
@@ -1,9 +1,20 @@
 document.querySelector('#menuConnexion').addEventListener("click", menuToggle);
+document.querySelector('.cross').addEventListener("click", crossToggle);
 
 function menuToggle() {
 
     document.querySelector('.menu').classList.toggle('menu-active');
-    document.querySelector('.john').classList.toggle('stick-active');
-    document.querySelector('.joe').classList.toggle('stick2-active');
+    document.querySelector('.cross').classList.toggle('cross-active');
+    document.querySelector('.stick').classList.toggle('stick-active');
+    document.querySelector('.stick2').classList.toggle('stick2-active');
+    document.querySelector('.fond').classList.toggle('fond-active');
+};
+
+function crossToggle(){
+    document.querySelector('.menu').classList.toggle('menu-active');
+    document.querySelector('.cross').classList.toggle('cross-active');
+    document.querySelector('.stick').classList.toggle('stick-active');
+    document.querySelector('.stick2').classList.toggle('stick2-active');
     document.querySelector('.fond').classList.toggle('fond-active');
 }
+
