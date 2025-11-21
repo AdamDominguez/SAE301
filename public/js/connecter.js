8e33e2ff3fff2d6770fe3b976089f21d8f2b234b
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2025-11-21 11:26:59 +0100

    Cross update

--- public/js/connecter.js
+++ public/js/connecter.js
@@ -1,5 +1,5 @@
 document.querySelector('#menuConnexion').addEventListener("click", menuToggle);
-document.querySelector('.cross').addEventListener("click", crossToggle);
+document.querySelector('.cross').addEventListener("click", menuToggle);
 document.querySelector('.fond').addEventListener("click", menuToggle);
 
 function menuToggle() {
@@ -10,11 +10,3 @@ function menuToggle() {
     document.querySelector('.stick2').classList.toggle('stick2-active');
     document.querySelector('.fond').classList.toggle('fond-active');
 };
-
-function crossToggle(){
-    document.querySelector('.menu').classList.toggle('menu-active');
-    document.querySelector('.cross').classList.toggle('cross-active');
-    document.querySelector('.stick').classList.toggle('stick-active');
-    document.querySelector('.stick2').classList.toggle('stick2-active');
-    document.querySelector('.fond').classList.toggle('fond-active');
-}
