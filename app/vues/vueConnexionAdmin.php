31363767599bb0484eff91d3fc0ff482de96b4fd
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2025-11-26 17:26:15 +0100

    cleaning up code, adding responsive elements and fixing positioned elements

--- app/vues/vueConnexionAdmin.php
+++ app/vues/vueConnexionAdmin.php
@@ -1,21 +1,18 @@
 <?php
 $title = "ACCUEIL";
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
     <link href="./public/css/main.css" rel="stylesheet">
-    <link href="./public/css/connexionadmin.css" rel="stylesheet">*
+    <link href="./public/css/connexionadmin.css" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="32x32" href="./public/img/favicons/favicon-32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./public/img/favicons/favicon-16.png">
 </head>
 
 <body>
-    <?php require "header.php"; ?>
     <main>
         <section class="ConnexionAdmin">
             <img src="./public/img/logo_admin.avif" alt="Logo Administrateur">
@@ -38,7 +35,6 @@ $title = "ACCUEIL";
             </form>
         </section>
     </main>
-    <?php require "footer.php"; ?>
 </body>
 
 </html>
