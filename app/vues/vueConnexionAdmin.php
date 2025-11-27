d7f8a6e55ef6e5d33f11b4f41bd387c3a27f5e61
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2025-11-27 09:38:03 +0100

    feat: admin panel login checkbox addition, forgot pswd addition and center align for text in connection tab

--- app/vues/vueConnexionAdmin.php
+++ app/vues/vueConnexionAdmin.php
@@ -3,6 +3,7 @@ $title = "Panel Administrateur | BeeLink";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title><?= $title ?></title>
@@ -14,6 +15,7 @@ $title = "Panel Administrateur | BeeLink";
 
 <body>
     <main>
+        <a href="index.php?action=accueil" class="BackAccueil">← Retour à la page d'accueil</a>
         <section class="ConnexionAdmin">
             <img src="./public/img/logo_admin.avif" alt="Logo Administrateur">
             <form method="POST">
@@ -32,6 +34,16 @@ $title = "Panel Administrateur | BeeLink";
                 </fieldset>
 
                 <button type="submit">CONNEXION</button>
+
+                <div class="ConnexionOpt">
+                    <div class="ConnexionCheck">
+                        <input type="checkbox" id="remember" name="remember" />
+                        <label for="remember">Se souvenir de moi</label>
+                    </div>
+                    <div class="ConnexionMDP">
+                        <a href="">Mot de passe oublié</a>
+                    </div>
+                </div>
             </form>
         </section>
     </main>
