ea90c93e70264ea444a430d1767ac87e982e7f3c
adam <laaraisadam22@gmail.com>
2025-12-04 14:40:28 +0100

    animations header sur les a

--- app/vues/footer.php
+++ app/vues/footer.php
@@ -8,9 +8,9 @@
 
         <div class="Footer2A">
             <h4>Nos partenaires</h4>
-            <a>IUT de Mulhouse</a>
-            <a>Département GEII</a>
-            <a>Département MMI</a>
+            <a href="">IUT de Mulhouse</a>
+            <a href="">Département GEII</a>
+            <a href="">Département MMI</a>
         </div>
 
         <div class="Footer1B">
