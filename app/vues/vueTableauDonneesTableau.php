d5a7f8c5e232fef063b5490baf2b7ec012bb300a
GAECHTER Clément <clementgaechter@gmail.com>
2025-12-15 17:53:56 +0100

    add: tableau de données

--- app/vues/vueTableauDonneesTableau.php
+++ app/vues/vueTableauDonneesTableau.php
@@ -57,9 +57,61 @@ $title = "Tableau de bord | BeeLink";
                 <a href="index.php?action=tableauDonneesTableau" class="onglet actif">Tableau</a>
             </nav>
 
-            <div class="grille-widgets-donnees">
-
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Temp. (°C)</th>
+                        <th>Poids (kg)</th>
+                        <th>Humidité (%)</th>
+                        <th>Fréq. (Hz)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>03/11/2024 20:00:00</td>
+                        <td>21°C</td>
+                        <td>15.6 kg</td>
+                        <td>82%</td>
+                        <td>233 Hz</td>
+                    </tr>
+                    <tr>
+                        <td>03/11/2024 08:00:00</td>
+                        <td>20°C</td>
+                        <td>15.8 kg</td>
+                        <td>80%</td>
+                        <td>242 Hz</td>
+                    </tr>
+                    <tr>
+                        <td>02/11/2024 20:00:00</td>
+                        <td>22°C</td>
+                        <td>15.6 kg</td>
+                        <td>85%</td>
+                        <td>244 Hz</td>
+                    </tr>
+                    <tr>
+                        <td>02/11/2024 08:00:00</td>
+                        <td>21°C</td>
+                        <td>15.4 kg</td>
+                        <td>83%</td>
+                        <td>205 Hz</td>
+                    </tr>
+                    <tr>
+                        <td>01/11/2024 20:00:00</td>
+                        <td>23°C</td>
+                        <td>15.7 kg</td>
+                        <td>80%</td>
+                        <td>222 Hz</td>
+                    </tr>
+                    <tr>
+                        <td>01/11/2024 08:00:00</td>
+                        <td>22°C</td>
+                        <td>15.5 kg</td>
+                        <td>81%</td>
+                        <td>213 Hz</td>
+                    </tr>
+                </tbody>
+            </table>
         </section>
     </main>
     <?php require "footer.php"; ?>
