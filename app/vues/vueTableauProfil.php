e6f279b660791126c63a2685376fd4cd882d5114
Axelb9 <axelbernier330@gmail.com>
2025-12-18 11:47:56 +0100

    Update vueTableauProfil.php
    Les infos de l'utilisateur sont récupérer en php. Il reste à coder la modification des champs si l'utilisateur veut changer ses infos personnelles.

--- app/vues/vueTableauProfil.php
+++ app/vues/vueTableauProfil.php
@@ -71,7 +71,7 @@ $title = "Tableau de bord | BeeLink";
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--jaune)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-phone w-4 h-4 text-[#E5B444]" aria-hidden="true">
                                     <path d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"></path>
                                 </svg>
-                                <span><?= $_SESSION['numero'] ?></span>
+                                <span><?= $_SESSION['numero'] ?? "Non renseigné" ?></span>
                             </div>
                             <div class="element-profil">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="var(--jaune)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin w-4 h-4 text-[#E5B444]" aria-hidden="true">
@@ -99,48 +99,48 @@ $title = "Tableau de bord | BeeLink";
                             <div class="flex-element info">
                                 <div class="element-info perso">
                                     <label class="prenom" for="firstname">Prénom<span style="color: var(--jaune);">*</span></label>
-                                    <input type="text" id="firstname" name="firstname" value="Jean">
+                                    <input type="text" id="firstname" name="firstname" value="<?= $_SESSION['acces'] ?>">
                                 </div>
                                 <div class="element-info perso-2">
                                     <label class="nom" for="lastname">Nom<span style="color: var(--jaune);">*</span></label>
-                                    <input type="text" id="lastname" name="lastname" value="Dupont">
+                                    <input type="text" id="lastname" name="lastname" value="<?= $_SESSION['nom'] ?>">
                                 </div>
                             </div>
 
                             <div class="element-info perso-3">
                                 <label class="email" for="email">Email<span style="color: var(--jaune);">*</span></label>
-                                <input type="text" id="email" name="email" value="test@gmail.com">
+                                <input type="text" id="email" name="email" value="<?=$_SESSION['email']?>">
                             </div>
 
                             <div class="element-info perso-4">
                                 <label class="mobile" for="mobile">Mobile</label>
-                                <input type="text" id="mobile" name="mobile" value="" placeholder="Veuillez rentrer votre numéro de téléphone...">
+                                <input type="text" id="mobile" name="mobile" value="<?= $_SESSION['numero'] ?? '' ?>" placeholder="Veuillez rentrer votre numéro de téléphone...">
                             </div>
 
                             <div class="element-info perso-5">
                                 <label class="adresse" for="adresse">Adresse<span style="color: var(--jaune);">*</span></label>
-                                <input type="text" id="adresse" name="adresse" value="123 Route de Provence">
+                                <input type="text" id="adresse" name="adresse" value="<?= $_SESSION['adresse'] ?>"> 
                             </div>
 
                             <div class="flex-element info-2">
                                 <div class="element-info perso-6">
                                     <label class="ville" for="ville">Ville<span style="color: var(--jaune);">*</span></label>
-                                    <input type="text" id="ville" name="ville" value="Aix-en-Provence">
+                                    <input type="text" id="ville" name="ville" value="<?=$_SESSION['ville']?>">
                                 </div>
                                 <div class="element-info perso-7">
                                     <label class="postal" for="postal">Code postal<span style="color: var(--jaune);">*</span></label>
-                                    <input type="text" id="postal" name="postal" value="13100">
+                                    <input type="text" id="postal" name="postal" value="<?=$_SESSION['postal']?>">
                                 </div>
                                 <div class="element-info perso-8">
                                     <label class="pays" for="pays">France<span style="color: var(--jaune);">*</span></label>
-                                    <input type="text" id="pays" name="pays" value="France">
+                                    <input type="text" id="pays" name="pays" value="<?= $_SESSION['pays'] ?>">
                                 </div>
                             </div>
 
-                            <div class="element-info perso-9">
+                           <!--  <div class="element-info perso-9">
                                 <label class="bio" for="bio">Biographie</label>
                                 <textarea type="text" id="bio" name="bio" placeholder="Veuillez rentrer votre description..."></textarea>
-                            </div>
+                            </div> -->
 
                             <div class="flex-bouton">
                                 <button class="enregistrer">
