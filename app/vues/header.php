995e1fb43534860c78bb9f1f6fd6d821798bd397
Adam DOMINGUEZ <adamdominguez68@gmail.com>
2026-01-21 16:53:30 +0100

    feat(tweaking of profile icon): made it so that once you're connected, the border radius of the dynamically changed profile pic gets border-radius 100% to make it fully round

--- app/vues/header.php
+++ app/vues/header.php
@@ -44,7 +44,7 @@ $tableauPage = strpos($pageActive, 'tableau') === 0;
     </div>
 
     <?php if (isset($_SESSION['acces'])): ?>
-        <div class="Connecter">
+        <div class="Connecter" style="border-radius: 100%">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#333333"
                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
