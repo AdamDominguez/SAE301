b5b5e73c81a0630b7b5dadc68b2fdaa8091ae3f2
AdamDOMINGUEZ <adamdominguez68@gmail.com>
2026-01-22 13:50:55 +0100

    fix: wrongly named notifications

--- app/vues/header.php
+++ app/vues/header.php
@@ -151,7 +151,7 @@ $tableauPage = strpos($pageActive, 'tableau') === 0 || $pageActive === 'inscript
             <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
             <polyline points="22 4 12 14.01 9 11.01"></polyline>
         </svg>
-        <span>Connexion réussie !</span>
+        <span>Traitement de demande réussie !</span>
     </div>
 </div>
 
@@ -165,7 +165,7 @@ $tableauPage = strpos($pageActive, 'tableau') === 0 || $pageActive === 'inscript
             <line x1="12" y1="8" x2="12" y2="12"></line>
             <line x1="12" y1="16" x2="12.01" y2="16"></line>
         </svg>
-        <span>Une erreur est survenue lors de la connexion.</span>
+        <span>Une erreur est survenue lors du traitement de la demande.</span>
     </div>
 </div>
 
